2ff55105a13e2708802e825ed871191a0be8dcaf
MarianC10 <marian_cons@yahoo.com>
2023-03-29 09:07:58 +0300

    Fixed some student tests

--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -8,8 +8,35 @@ import validation.NotaValidator;
 import validation.StudentValidator;
 import validation.TemaValidator;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class StudentTest {
     private Service service;
+    private String studentContents ="<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?>\n" +
+            "<Entitati>\n" +
+            "    <student ID=\"1\">\n" +
+            "        <Nume>numenou</Nume>\n" +
+            "        <Grupa>222</Grupa>\n" +
+            "    </student>\n" +
+            "    <student ID=\"2\">\n" +
+            "        <Nume>Maria</Nume>\n" +
+            "        <Grupa>222</Grupa>\n" +
+            "    </student>\n" +
+            "    <student ID=\"3\">\n" +
+            "        <Nume>John</Nume>\n" +
+            "        <Grupa>231</Grupa>\n" +
+            "    </student>\n" +
+            "    <student ID=\"4\">\n" +
+            "        <Nume>Ion</Nume>\n" +
+            "        <Grupa>227</Grupa>\n" +
+            "    </student>\n" +
+            "    <student ID=\"5\">\n" +
+            "        <Nume>student</Nume>\n" +
+            "        <Grupa>932</Grupa>\n" +
+            "    </student>\n" +
+            "</Entitati>\n";
 
     @Before
     public void setUp() {
@@ -17,10 +44,24 @@ public class StudentTest {
         var assignmentValidator = new TemaValidator();
         var gradeValidator = new NotaValidator();
 
+        var file = new File("test-students.xml");
+        FileOutputStream fooStream;
+        try {
+            fooStream = new FileOutputStream(file, false);
+
+            byte[] myBytes = studentContents.getBytes();
+            fooStream.write(myBytes);
+            fooStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         var studentRepository = new StudentXMLRepository(studentValidator, "test-students.xml");
         var assignmentRepository = new TemaXMLRepository(assignmentValidator, "test-assignments.xml");
         var gradeRepository = new NotaXMLRepository(gradeValidator, "test-grades.xml");
         service = new Service(studentRepository, assignmentRepository, gradeRepository);
+
     }
 
     @Test
@@ -194,7 +235,7 @@ public class StudentTest {
     @Test
     public void bva_2_AddStudentValidGroup() {
         // Arrange
-        var id = "10";
+        var id = "11";
         var name= "name";
         var group = 111;
 
@@ -208,7 +249,7 @@ public class StudentTest {
     @Test
     public void bva_3_AddStudentValidGroup() {
         // Arrange
-        var id = "10";
+        var id = "12";
         var name= "name";
         var group = 111;
 
@@ -222,7 +263,7 @@ public class StudentTest {
     @Test
     public void bva_4_AddStudentValidGroup() {
         // Arrange
-        var id = "10";
+        var id = "13";
         var name= "name";
         var group = 936;
 
@@ -236,7 +277,7 @@ public class StudentTest {
     @Test
     public void bva_5_AddStudentValidGroup() {
         // Arrange
-        var id = "10";
+        var id = "14";
         var name= "name";
         var group = 937;
 
@@ -292,7 +333,7 @@ public class StudentTest {
     @Test
     public void bva_9_AddStudentValidId() {
         // Arrange
-        var id = "10";
+        var id = "15";
         var name= "name";
         var group = 111;
 
@@ -306,7 +347,7 @@ public class StudentTest {
     @Test
     public void bva_10_AddStudentInvalidName() {
         // Arrange
-        var id = "10";
+        var id = "100";
         String name = null;
         var group = 111;
 
@@ -320,7 +361,7 @@ public class StudentTest {
     @Test
     public void bva_11_AddStudentInvalidName() {
         // Arrange
-        var id = "10";
+        var id = "100";
         var name = "";
         var group = 111;
 
@@ -334,7 +375,7 @@ public class StudentTest {
     @Test
     public void bva_12_AddStudentValidName() {
         // Arrange
-        var id = "10";
+        var id = "16";
         var name = "test";
         var group = 111;
 
